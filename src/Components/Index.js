ce94f04cd161a1570abec5898b6608a84508ac55
Jesse Songok <jessesongok@gmail.com>
2019-03-05 12:59:02 +0300

    Added Components

--- src/Components/Index.js
+++ src/Components/Index.js
@@ -3,8 +3,10 @@ import Pic from '../me.svg';
 class Index extends Component {
     render() {
         return (
-            <div className="container">
-                <img src={Pic} alt="pic" className="img-fluid img-thumbnail rounded-circle"/>
+            <div className="imgradient">
+                <div className="container">
+                    <img src={Pic} alt="pic" className="my-4 myimage img-fluid img-thumbnail rounded-circle" />
+                </div>
             </div>
         );
     }
